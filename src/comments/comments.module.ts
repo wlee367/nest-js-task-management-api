5d40a4dd251bc351a40e5cc648c5f8f69176bfa7
Won Ryul Lee <wlee367@uwo.ca>
2020-05-17 22:25:03 -0400

    feature: when user creates a comment - db records a row in UserActivity Table saying user created a comment on task

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -3,11 +3,24 @@ import { CommentsController } from './comments.controller';
 import { CommentsService } from './comments.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommentRepository } from './comments.repository';
-import { AuthModule } from 'src/auth/auth.module';
+import { AuthModule } from '../auth/auth.module';
+import { UserActivityModule } from 'src/user-activity/user-activity.module';
+import { TasksModule } from 'src/tasks/tasks.module';
+import { UserActivityRepository } from 'src/user-activity/user-activity.repository';
+import { TaskRepository } from 'src/tasks/tasks.repository';
 
 @Module({
   controllers: [CommentsController],
   providers: [CommentsService],
-  imports: [TypeOrmModule.forFeature([CommentRepository]), AuthModule],
+  imports: [
+    TypeOrmModule.forFeature([
+      CommentRepository,
+      UserActivityRepository,
+      TaskRepository,
+    ]),
+    AuthModule,
+    UserActivityModule,
+    TasksModule,
+  ],
 })
 export class CommentsModule {}
--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -8,12 +8,19 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/auth/user.entity';
 import { Comment } from './comments.entity';
 import { CreateCommentDTO } from './dto/create-comments.dto';
+import { UserActivityRepository } from 'src/user-activity/user-activity.repository';
+import { CreateUserActivityDTO } from 'src/user-activity/dto/createUserActivityDTO';
+import { TaskRepository } from 'src/tasks/tasks.repository';
 
 @Injectable()
 export class CommentsService {
   constructor(
     @InjectRepository(CommentRepository)
     private commentsRepository: CommentRepository,
+    @InjectRepository(UserActivityRepository)
+    private userActivityRepository: UserActivityRepository,
+    @InjectRepository(TaskRepository)
+    private taskRepository: TaskRepository,
   ) {}
 
   async getCommentById(id: string, user: User): Promise<Comment> {
@@ -35,6 +42,14 @@ export class CommentsService {
     createCommentDto: CreateCommentDTO,
     user: User,
   ): Promise<Comment> {
+    const { taskId } = createCommentDto;
+
+    const task = await this.taskRepository.findOne({ where: { id: taskId } });
+
+    const createUserActivityDTO = new CreateUserActivityDTO();
+    createUserActivityDTO.user = user;
+    createUserActivityDTO.description = `${user.username} commented on: ${task.title}`;
+    await this.userActivityRepository.createUserActivity(createUserActivityDTO);
     return this.commentsRepository.createComment(createCommentDto, user);
   }
 
